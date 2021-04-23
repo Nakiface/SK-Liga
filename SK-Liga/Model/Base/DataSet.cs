36f2c5b9e2d27200232f8d51a88ff76e3a49129a
Bernhard Schmitt <BernhardSchmitt@astendo.de>
2021-04-23 18:24:35 +0200

    [#1] added DataSet class

---
+++ SK-Liga/Model/Base/DataSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK_Liga.Model.Base
+{
+    public class DataSet
+    {
+        public List<Match> Matches { get; set; }
+        public List<PlayerElos> PlayerElos { get; set; }
+
+        public DataSet(List<Match> matches, List<PlayerElos> playerElos)
+        {
+            this.Matches = matches;
+            this.PlayerElos = playerElos;
+        }
+    }
+}
