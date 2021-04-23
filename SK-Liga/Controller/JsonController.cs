30a25756437e7b88c3347ea0cf8c6a734e4b192f
Bernhard Schmitt <BernhardSchmitt@astendo.de>
2021-04-23 17:54:12 +0200

    [#2] Json Controller created

---
+++ SK-Liga/Controller/JsonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK_Liga.Controller
+{
+    public static class JsonController
+    {
+        public static void Save(string path, object context)
+        {
+
+        }
+    }
+}
