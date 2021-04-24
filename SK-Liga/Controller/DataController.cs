e975440c1ea717cdc43c1ccb61641673efec69a2
Bernhard Schmitt <BernhardSchmitt@astendo.de>
2021-04-24 22:10:13 +0200

    [#6] create dummy json function

--- SK-Liga/Controller/DataController.cs
+++ SK-Liga/Controller/DataController.cs
@@ -1,4 +1,5 @@
 ﻿using SK_Liga.Model.Base;
+using SK_Liga.Model.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,10 @@ namespace SK_Liga.Controller
 {
     public class DataController
     {
-        public List<Match> Matches { get; set; }
-        public List<PlayerElos> PlayerElos { get; set; }
-        public List<CurrentPlayer> CurrentPlayers { get; set; }
-        public List<Game> GameList { get; set; }
+        public List<Match> Matches { get; set; } = new List<Match>();
+        public List<PlayerElos> PlayerElos { get; set; } = new List<PlayerElos>();
+        public List<CurrentPlayer> CurrentPlayers { get; set; } = new List<CurrentPlayer>();
+        public List<Game> GameList { get; set; } = new List<Game>();
 
         public DataController ()
         {
@@ -21,11 +22,11 @@ namespace SK_Liga.Controller
 
         private void LoadData()
         {
-            var dataJsonPath = ConfigController.LoadConfig().DataPath;
-            DataSet dataSetOrigin = JsonController.Load(dataJsonPath) as DataSet;
+            var config = ConfigController.LoadConfig();
+            DataSet dataSetOrigin = JsonController.LoadData(config.DataPath);
             this.Matches = dataSetOrigin.Matches;
             this.PlayerElos = dataSetOrigin.PlayerElos;
-            this.GameList = dataSetOrigin.gameList;
+            this.GameList = dataSetOrigin.GameList;
         }
 
         public void CreateCurrentPlayer(Game game)
@@ -34,9 +35,32 @@ namespace SK_Liga.Controller
             foreach (var player in PlayerElos)
             {
                 var gameElo = player.EloList.Where(el => el.Game.Equals(game)).FirstOrDefault();
-                CurrentPlayer currentPlayer = new CurrentPlayer(player.Name, gameElo.Elo);
+                CurrentPlayer currentPlayer = new CurrentPlayer();
+                currentPlayer.Load(player, game);
             }
             this.CurrentPlayers = currentPlayers;
+        }
+
+        public static void CreateFirstDataSetJson()
+        {
+            Game game = new Game("dummyGame");
+            GameElo gameElo1 = new GameElo(game, 1000);
+            GameElo gameElo2 = new GameElo(game, 500);
+
+            PlayerElos playerElosDummy1 = new PlayerElos("Dummy", new List<GameElo>() { gameElo1 });
+            PlayerElos playerElosDummy2 = new PlayerElos("Dummy2", new List<GameElo>() { gameElo2 });
+
+            CurrentPlayer currentPlayer1 = new CurrentPlayer();
+            currentPlayer1.Load(playerElosDummy1, game);
+            CurrentPlayer currentPlayer2 = new CurrentPlayer();
+            currentPlayer2.Load(playerElosDummy2, game);
+
+            Match match = new Match(new List<CurrentPlayer>() { currentPlayer1 }, new List<CurrentPlayer>() { currentPlayer2 }, game);
+
+            DataSet dataSet = new DataSet(new List<Match>() { match }, new List<PlayerElos>() { playerElosDummy1, playerElosDummy2 });
+
+            Config config = ConfigController.LoadConfig();
+            JsonController.Save(config.DataPath, dataSet);
         }
     }
 }
